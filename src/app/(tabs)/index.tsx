83fef7466f6b211fc7f67936fb4f542e9824eb39
David <dwander616@gmail.com>
2025-01-01 16:55:21 -0300

    criando arquivo principal

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <View>
       <Text>
+<<<<<<< HEAD
         Welcome to React Native!
+=======
+        Welcome to the Home Screen!
+>>>>>>> 6bf7cc9 (criando arquivo principal)
       </Text>
     </View>
   )
