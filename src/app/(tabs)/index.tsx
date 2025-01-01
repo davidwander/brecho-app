2aa38a0c11c4afb80822004463707de2990b1285
David <dwander616@gmail.com>
2025-01-01 20:43:50 -0300

    add components: loading, input, avatar.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
 import { View, Text } from "react-native"
 
+import { Input } from "@/components/input"
+import { MenuButton } from "@/components/menu-button"
+import { Avatar } from "@/components/avatar"
+
 export default function Home() {
   return (
-    <View className="flex-1 bg-gray-900">
-      <Text className="">
-        Welcome to React Native!
-      </Text>
+    <View className="flex-1 bg-gray-900 pt-16 p-4">
+      <Input>
+        <MenuButton />
+        <Input.Field placeholder="Pesquisar clientes" />
+        <Avatar 
+          source={{ uri: "https://github.com/davidwander.png" }} 
+          size="medium"
+        />
+      </Input>
     </View>
   )
 }
