cfda8c15a9564cf571046e936e6d4994528b4237
David <dwander616@gmail.com>
2025-01-01 18:43:53 -0300

    add e config nativewind, add cores.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,13 +2,9 @@ import { View, Text } from "react-native"
 
 export default function Home() {
   return (
-    <View>
-      <Text>
-<<<<<<< HEAD
+    <View className="flex-1 bg-gray-900">
+      <Text className="">
         Welcome to React Native!
-=======
-        Welcome to the Home Screen!
->>>>>>> 6bf7cc9 (criando arquivo principal)
       </Text>
     </View>
   )
---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import "@/styles/global.css"
+
+import { Slot } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <Slot />
+    </GestureHandlerRootView>
+  )
+}
